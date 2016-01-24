01550d6527fa8d5cc1f36fe9134795cc7ff85fe9
gmanpark <gmanpark@tmon.co.kr>
2016-01-25 00:20:08 +0900

    [TASK] angular2 route

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,55 +1,41 @@
 import {Component} from 'angular2/core';
-import {Gman} from './gman';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {GmanComponent} from "./gman";
 
-interface Hero {
-    id: number;
-    name: string;
-}
-
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
 
 @Component({
     selector: 'my-app',
-    template:`
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
-      <ul>
-        <li *ngFor="#hero of heroes">
-            <span (click)="gman.onSelect(hero)">{{hero.name}}</span>
-        </li>
-        <img src="./img/1.jpg" width="50" height="50">
-      </ul>
-    </div>
-    `
+    templateUrl: './html/app.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 
+@RouteConfig([
+    { path: '/GmanDetail', component: GmanComponent, name: 'GmanDetail' }
+])
+
 export class AppComponent {
-    public title = 'Tour of Heroes';
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
 
-    private heroes = HEROES;
-    public gman = new Gman();
-
-    onSelect(hero){
-        console.log("appComponent")
-    }
+    //
+    //ngOnInit() {
+    //    console.log("ngOnInit");
+    //}
+    //ngOnDestroy() {
+    //    console.log("ngOnDestroy");
+    //}
+    //ngDoCheck() {
+    //    console.log("ngDoCheck");
+    //}
+    //ngOnChanges(changes) {
+    //    console.log("ngOnChanges"+ changes);
+    //}
+    //ngAfterContentInit() {
+    //    console.log("ngAfterContentInit");
+    //}
+    //ngAfterContentChecked() {
+    //    console.log("ngAfterContentChecked");    }
+    //ngAfterViewInit() {
+    //    console.log("ngAfterViewInit");    }
+    //ngAfterViewChecked() {
+    //    console.log("ngAfterViewChecked");    }
 }
 
--- src/main/resources/static/app/gman.ts
+++ src/main/resources/static/app/gman.ts
@@ -1,7 +1,45 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 
-export class Gman{
-    public onSelect(){
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './html/gman.html'
+})
+
+export class GmanComponent implements OnInit{
+    constructor(){
+        console.log("testt");
+    }
+
+    public onSelect() {
+        alert("test");
         debugger;
+        return false;
+    }
+
+    ngOnInit() {
+        console.log("ngOnInit");
+    }
+
+    ngOnDestroy() {
+        console.log("ngOnDestroy");
+    }
+    ngDoCheck() {
+        console.log("ngDoCheck");
+    }
+    ngOnChanges(changes) {
+        console.log("ngOnChanges"+ changes);
+    }
+    ngAfterContentInit() {
+        console.log("ngAfterContentInit");
+    }
+    ngAfterContentChecked() {
+        console.log("ngAfterContentChecked");
+    }
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit");
+    }
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked");
     }
 }
