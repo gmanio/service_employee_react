692cbb62ee35fc10e2bb4291d3cc638e77040975
gmanpark <gmanpark@tmon.co.kr>
2016-03-28 23:50:32 +0900

    [TASK] init

--- src/main/resources/static/boot.ts
+++
@@ -1,9 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import AppComponent from './app/app';
-
-bootstrap(AppComponent, [
-    HTTP_PROVIDERS,
-    ROUTER_PROVIDERS
-]);
